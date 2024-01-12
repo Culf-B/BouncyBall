cac36c1cf2fbb2bc0f3c4c7d0afed4ea50bb8d73
Culf-B <49363745+Culf-B@users.noreply.github.com>
2024-01-12 13:02:04 +0100

    More balls and balls are random and change color on bounce

--- sketch.js
+++ sketch.js
@@ -1,28 +1,40 @@
-const SW = 400;
-const SH = 400;
+const SW = 600;
+const SH = 700;
+balls = [];
 
-function setup() {
-  createCanvas(SW, SH);
-  b = new Bold(200, 200, random(-10, 10), random(-10, 10), 20, 100, 0, 200);
-}
-
-function draw() {
-  background(220);
-  b.update();
-}
-
-function Bold(xPosStart, yPosStart, xSpeed, ySpeed, radius, farveR, farveG, farveB)
+function setup()
 {
-  this.x = xPosStart;
-  this.y = yPosStart;
-  this.xSpeed = xSpeed;
-  this.ySpeed = ySpeed;
-  this.radius = radius;
-  this.farveR = farveR;
-  this.farveG = farveG;
-  this.farveB = farveB;
+  createCanvas(SW, SH);
+  for (var i = 0; i < 50; i++)
+  {
+    balls.push(new Ball());
+  }
+}
 
-  this.update = function()
+function draw()
+{
+  background(220);
+  balls.forEach(ball =>
+  {
+    ball.update();
+  });
+}
+
+class Ball
+{
+  constructor()
+  {
+    this.radius = random(5, 30);
+    this.x = random(0 + this.radius, SW - this.radius);
+    this.y = random(0 + this.radius, SH - this.radius);
+    this.xSpeed = random(-10, 10);
+    this.ySpeed = random(-10, 10);
+    this.farveR = random(0, 255);
+    this.farveG = random(0, 255);
+    this.farveB = random(0, 255);
+  }
+
+  update()
   {
     // Update position
     this.x = this.x + this.xSpeed;
@@ -33,26 +45,36 @@ function Bold(xPosStart, yPosStart, xSpeed, ySpeed, radius, farveR, farveG, farv
     {
       this.x = SW - this.radius;
       this.xSpeed = this.xSpeed *-1;
+      this.changeColor();
     }
     // Collision detection X left
     if (this.x - this.radius < 0)
     {
       this.x = this.radius;
       this.xSpeed = this.xSpeed *-1;
+      this.changeColor();
     }
     // Collision detection Y bottom
     if (this.y + this.radius > SH)
     {
       this.y = SH - this.radius;
       this.ySpeed = this.ySpeed *-1;
+      this.changeColor();
     }
     // Collision detection Y top
     if (this.y - this.radius < 0)
     {
       this.y = this.radius;
       this.ySpeed = this.ySpeed *-1;
+      this.changeColor();
     }
     fill(this.farveR, this.farveG, this.farveB);
     circle(this.x, this.y, this.radius * 2);
-  };
+  }
+  changeColor()
+  {
+    this.farveR = random(0, 255);
+    this.farveG = random(0, 255);
+    this.farveB = random(0, 255);
+  }
 }
